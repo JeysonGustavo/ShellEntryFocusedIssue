ae3eb25897d5c877e6e09251785e56fcbb6b6e4b
Jeyson Gomes <jeyson.gomes@stechgen.com>
2019-10-11 15:25:48 -0300

    Xaminals Example with search bar focus issue

---
+++ Xaminals/Views/MonkeysPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using Xamarin.Forms;
+using Xaminals.Models;
+
+namespace Xaminals.Views
+{
+    public partial class MonkeysPage : ContentPage
+    {
+        public MonkeysPage()
+        {
+            InitializeComponent();
+
+            Test.Focused += Test_Focused;
+        }
+
+        async void OnCollectionViewSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string monkeyName = (e.CurrentSelection.FirstOrDefault() as Animal).Name;
+            // This works because route names are unique in this application.
+            await Shell.Current.GoToAsync($"monkeydetails?name={monkeyName}");
+            // The full route is shown below.
+            // await Shell.Current.GoToAsync($"//animals/monkeys/monkeydetails?name={monkeyName}");
+        }
+
+        //The event Test_Focused is not called when the search bar gotfocus.
+        public void Test_Focused(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
