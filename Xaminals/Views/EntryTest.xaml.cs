49af440a4160b1eb6198ae8631429c2fe3043f4f
Jeyson Gomes <jeyson.gomes@stechgen.com>
2019-10-25 11:08:01 -0300

    Example of Shell entry focused event issue

---
+++ Xaminals/Views/EntryTest.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Xaminals.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class EntryTest : ContentPage
+    {
+        public EntryTest()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+        }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+
+            entryOne.Text = "";
+            entryOne.Unfocus();
+        }
+
+        private void entryOne_Focused(object sender, FocusEventArgs e)
+        {
+
+        }
+
+        private void entryTwo_Focused(object sender, FocusEventArgs e)
+        {
+
+        }
+    }
+}
